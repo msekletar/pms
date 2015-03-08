ed4a5c1d15631de4394a883f6eee659a3b10075a
Michal Sekletar <msekleta@redhat.com>
2015-03-08 15:28:57 +0100

    Implement shell functions for generating input of pms
    Input of pipeline merge sort must be always power of 2. Number of processors
    used is then log_2(input_size) + 1.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function usage() {
+usage() {
     printf "usage: test.sh COUNT\n\n"
     printf "Start a program pms. An example implementation of Pipeline Merge Sort algorithm.\n"
     printf "To run the program it is necessary to have OpenMPI installed.\n\n"
@@ -9,7 +9,56 @@ function usage() {
     printf "          of 2, otherwise it will be rounded to next bigger power of 2)\n"
 }
 
-if [ "$#" != "1" ]; then
+generate_numbers() {
+    local source="/dev/urandom"
+    local filename="numbers"
+
+    if [ -z "$1" ]; then
+        echo "generate_numbers requires exactly one argument" 1>&2
+        exit 1
+    fi
+
+    rm -f "$filename" >/dev/null 2>&1
+
+    dd if="$source" of="$filename" bs=1 count="$1" >/dev/null 2>&1
+}
+
+is_pow_2() {
+    declare -i r
+
+    if [ -z "$1" ]; then
+        echo "is_pow_2 requires exactly one argument" 1>&2
+        exit 1
+    fi
+
+    r=$(echo "is_pow_2($1)" | bc -lq pow2.bc 2>/dev/null)
+    return $r
+}
+
+next_pow_2() {
+    declare -i r
+
+    if [ -z "$1" ]; then
+        echo "next_pow_2 requires exactly one argument" 1>&2
+        exit 1
+    fi
+
+    r=$(echo "next_pow_2($1)" | bc -lq pow2.bc 2>/dev/null)
+    echo "$r"
+}
+
+if [ -z "$1" ]; then
     usage
     exit 1
 fi
+
+count="$1"
+is_pow_2 "$count"
+
+if [ "$?" != "1" ]; then
+    count=$(next_pow_2 "$count")
+fi
+
+generate_numbers "$count"
+
+make -s
